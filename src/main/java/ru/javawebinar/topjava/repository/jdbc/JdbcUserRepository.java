e9830fedc69fed567d7b8519b11cae5b32d9c628
Александр <maleks1983@gmail.com>
2019-11-28 00:48:42 +0200

    3 13 fix servlet

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -8,10 +8,12 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.List;
+
 
 @Repository
 public class JdbcUserRepository implements UserRepository {
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +11,12 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id,datetime, description, calories) VALUES
+    (100000,'2015-05-30 10:10', 'Завтрак', '500'),
+    (100000,'2015-05-30 13:15', 'Обед', '1000'),
+    (100000,'2015-05-30 20:25', 'Ужин', '500'),
+    (100001,'2015-05-31 10:59', 'Завтрак', '1000'),
+    (100001,'2015-05-31 13:06', 'Обед', '500'),
+    (100001,'2015-05-31 20:07', 'Ужин', '510');
+
