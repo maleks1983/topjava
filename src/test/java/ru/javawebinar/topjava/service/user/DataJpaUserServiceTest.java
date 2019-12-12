165db28e76af52a2c613b772e14a2ccd189c38fc
Александр <maleks1983@gmail.com>
2019-12-12 16:55:28 +0200

    5 9 spring cache

--- src/test/java/ru/javawebinar/topjava/service/user/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/user/DataJpaUserServiceTest.java
@@ -1,9 +1,8 @@
 package ru.javawebinar.topjava.service.user;
 
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.ActiveDbProfileResolver;
 import ru.javawebinar.topjava.Profiles;
-import ru.javawebinar.topjava.service.meal.MealServiceTest;
+
 
 @ActiveProfiles(value={Profiles.HSQL_DB,Profiles.DATAJPA})
 
